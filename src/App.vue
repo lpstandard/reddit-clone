f0334e9c5ca6f6bd42768c8b97760c8bdb9aa438
Tony Kung <lps.tony@gmail.com>
2020-02-18 21:33:09 +0800

    2nd commit

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">/r/rarepuppers</a>
+    </nav>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App"
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Posts.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+<ul class="list-unstyled">
+  <li class="media">
+    <img src="http://www.w3.org/2000/svg" class="mr-3" alt="...">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">List-based media object</h5>
+      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    </div>
+  </li>
+</ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Posts",
+  data() {
+    return {
+      posts: []
+    };
+  },
+  mounted() {
+    this.load();
+  },
+  methods: {
+    load() {
+      const url = "https://www.reddit.com/r/MovieDetails/.json";
+      fetch(url)
+        .then(response => response.json())
+        .then(result => {
+          this.posts = result.data.children;
+        });
+    }
+  }
+};
+</script>
+
+</style>
